bfc8a713a75520021a84e69e7dbe295ea108a082
Brandon <brandondong604@hotmail.com>
2017-04-27 21:52:54 -0700

    Initial commit

---
+++ POELiveSplitComponent/UI/POEComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml;
+using LiveSplit.Model;
+using LiveSplit.UI;
+using LiveSplit.UI.Components;
+
+namespace POELiveSplitComponent.UI
+{
+    internal class POEComponent : LogicComponent
+    {
+        private LiveSplitState state;
+
+        public POEComponent(LiveSplitState state)
+        {
+            this.state = state;
+        }
+
+        public override string ComponentName => "POE";
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override XmlNode GetSettings(XmlDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Control GetSettingsControl(LayoutMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetSettings(XmlNode settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        { }
+    }
+}
---
+++ POELiveSplitComponent/UI/POEComponentFactory.cs
@@ -0,0 +1,52 @@
+﻿using LiveSplit.UI.Components;
+using System;
+using LiveSplit.Model;
+
+namespace POELiveSplitComponent.UI
+{
+    class POEComponentFactory : IComponentFactory
+    {
+        public ComponentCategory Category => ComponentCategory.Timer;
+
+        public string ComponentName => "POE";
+
+        public string Description => "Load Time Removal and Auto Splitting for Path of Exile.";
+
+        public string UpdateName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string UpdateURL
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string XMLURL
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new POEComponent(state);
+        }
+    }
+}
